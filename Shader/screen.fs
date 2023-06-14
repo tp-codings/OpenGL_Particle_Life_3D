5718f532de0ec26362ad5263d5ce5afe1ecd67c9
Tobias P <tp.codingprofiles@gmail.com>
2023-06-14 14:35:13 +0200

    Added all comments

--- Shader/screen.fs
+++ Shader/screen.fs
@@ -5,6 +5,7 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+//Offset um Fragment
 const float offset = 1.0 / 300.0;
 
 vec3 col = vec3(0.0);
@@ -44,12 +45,6 @@ void main()
 		1,  1, 1
 	);
 
-	float dafuqKernel[9] = float[](
-		-2,  -1, 0,
-		-1, 1, 1, 
-		1,  1, 2
-	);
-
 	vec3 sampleTex[9];
 
 	for(int i= 0; i<9; i++)
@@ -61,14 +56,17 @@ void main()
 	switch(choice)
 	{
 		case 0:
+		//Sharpness
 			for(int i = 0; i<9; i++)
 				col += sampleTex[i] * sharpnessKernel[i];
 			break;
 		case 1:
+		//Blur
 			for(int i = 0; i<9; i++)
 				col += sampleTex[i] * blurKernel[i];
 			break;
 		case 2:
+		//Edge detection
 			for(int i = 0; i<9; i++)
 				col += sampleTex[i] * edgeDetectionKernel[i];
 			break;
@@ -82,10 +80,6 @@ void main()
 			FragColor = vec4(col, 1.0);
 			float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
 			col = vec3(average, average, average);
-			break;
-		case 5:
-			for(int i = 0; i<9; i++)
-				col += sampleTex[i] * dafuqKernel[i];
 			break;
 		default:
 			col = vec3(texture(screenTexture, TexCoords));
